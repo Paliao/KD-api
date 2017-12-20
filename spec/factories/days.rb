94ef089ab6c0ae8b1cec0e6cb321b36f04d2a4d8
Andre Paliao <andre_paliao@hotmail.com>
2017-12-20 21:14:07 -0200

    Taking off spec to facilitate

--- spec/factories/days.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :day do
-    day "MyString"
-    oppened false
-    open_hour "2017-12-20 19:29:48"
-    closure_hour "2017-12-20 19:29:48"
-    special false
-  end
-end
--- spec/factories/establishments.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :establishment do
-    name "MyString"
-    address "MyString"
-    latitude 1
-    longitude 1
-    rating 1.5
-  end
-end
