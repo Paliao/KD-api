37e310eb7308d6f60329c6e29f24c2f94f2443ba
Paliao <andre_paliao@hotmail.com>
2018-03-16 12:27:03 -0300

    Creating store intiial reducer

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View } from 'react-native'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
+
+import reducers from './src/reducers'
+import RootStack from './src/routes'
+
+const store = applyMiddleware(thunk)(createStore)(reducers)
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <Provider store={store}>
+        <View style={{flex: 1}}>
+          <RootStack />
+        </View>
+      </Provider>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
